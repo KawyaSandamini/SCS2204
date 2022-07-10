d434f17606f795f966438821c9158687ade11b41
Kawya Sandamini <2020cs085@stu.ucsc.cmb.ac.lk>
2022-07-10 18:31:16 +0530

    Done

--- Q3Part2.scala
+++ Q3Part2.scala
@@ -1,35 +1,3 @@
-/*class int(o:Int){
-    var p=o
-    def ++():Int={
-        var q=p
-        p=p+1
-        q
-    }
-    def --():Int={
-        var q=p
-        p=p-1
-        q
-    }
-    override def toString(): String = p.toString
-    def v():Int=p.toInt
-}
-def ++(o:Int):Int=o+1
-def --(o:Int):Int=o-1
-
-@main
-def run()={
-    var (a,b,c,d)=(2,3,4,5)
-    var k=4.3f
-    var g=4.0f
-    
-    println(--(b)*a.v() + c.v() * d.--())
-    b-=1
-    println(a.++())
-    println(-2*(g-k)+c.v())
-    println(c.++())
-    println(++(c.v())*a.++())
-
-}*/
 var (a,b,c,d) = (2,3,4,5)
 var k:Float=4.3f
 var g:Float=4.0f
